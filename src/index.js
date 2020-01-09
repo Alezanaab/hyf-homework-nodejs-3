4c139ebc4ceb7c4da16393961aa41d516c0fe98c
AlezanaAbrego <aabrego@unitedvirtualities.com>
2020-01-09 13:44:01 -0700

    Feat: Add GET /users, POST /user, GET /user/:id

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 
+var users = [];
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+
 app.get('/users', function (req, res) {
-    res.send([]);
+    res.send(users);
+    users[0] = { id: 0 };
+});
+
+app.post('/user', function (req, res) {
+    res.status(200).send({ id: 0 });
+});
+
+app.get('/user/:id', function (req, res) {
+    res.send({ id: 0 });
 });
 
 
